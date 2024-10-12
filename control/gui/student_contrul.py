155d1fa72d972973a6471dc851473d9f6b06c1fd
elliott <elliott.cn.lee@gmail.com>
2024-10-12 21:01:22 +1100

    commit all

--- control/gui/student_contrul.py
+++ control/gui/student_contrul.py
@@ -9,18 +9,14 @@ from util.constant import Constant
 def show_operation_menu(root, student_service, subject_service):
     root.title("Welcome")
 
-    # 清空当前窗口的内容
     for widget in root.winfo_children():
         widget.destroy()
 
-    # 创建新的操作按钮
     tk.Label(root, text="Welcome! Student Course Menu").pack(pady=(10, 5))
 
-    # 创建一个框架用于平行排列按钮
     button_frame = tk.Frame(root)
     button_frame.pack(pady=5)  # 在框架和顶部留出一些空间
 
-    # 创建操作按钮，平行排列
     button1 = tk.Button(button_frame, text="Change Password",
                         command=lambda: show_change_password_menu(root, student_service, subject_service))
     button1.pack(side=tk.LEFT, padx=5)
@@ -39,20 +35,16 @@ def show_operation_menu(root, student_service, subject_service):
 
 def show_subjects(root, student_service, subject_service):
     try:
-        # 查询课程列表
         lists = subject_service.query_subjects()
 
-        # 清空当前窗口的内容
         for widget in root.winfo_children():
             widget.destroy()
 
         tk.Label(root, text="Subjects List").pack(pady=(10, 5))
 
-        # 创建一个文本框来显示课程列表
         subjects_text = tk.Text(root, width=50, height=15)
         subjects_text.pack(pady=(5, 5))
 
-        # 将课程列表添加到文本框
         if lists:
             for subject in lists:
                 subjects_text.insert(tk.END, subject)
@@ -61,7 +53,6 @@ def show_subjects(root, student_service, subject_service):
         else:
             subjects_text.insert(tk.END, "No subjects available.")
 
-        # 创建返回主菜单按钮
         back_button = tk.Button(root, text="Back to Menu",
                                 command=lambda: show_operation_menu(root, student_service, subject_service))
         back_button.pack(pady=(5, 5))
@@ -84,7 +75,6 @@ def enroll_subject(root, subject_service):
 
 
 def show_remove_subject_page(root, student_service, subject_service):
-    # 清空当前窗口的内容
     for widget in root.winfo_children():
         widget.destroy()
 
@@ -93,14 +83,14 @@ def show_remove_subject_page(root, student_service, subject_service):
     tk.Label(root, text="Subject ID").pack(pady=(5, 0))
     subject_id_entry = tk.Entry(root)
     subject_id_entry.pack(pady=(0, 5))
-    subject_id_entry.focus_set()  # 设置焦点到新密码输入框
+    subject_id_entry.focus_set()
 
     def confirm_remove_subject():
         try:
             subject_id = subject_id_entry.get()
             if subject_id:
                 subject_service.remove_subject(subject_id)
-                show_operation_menu(root, student_service, subject_service)  # 返回主菜单
+                show_operation_menu(root, student_service, subject_service)
             else:
                 messagebox.showerror("Error", "Incorrect input, please try again.")
         except Exception as e:
@@ -109,24 +99,21 @@ def show_remove_subject_page(root, student_service, subject_service):
     confirm_button = tk.Button(root, text="Submit", command=confirm_remove_subject)
     confirm_button.pack(pady=(10, 5))
 
-    # 返回主菜单按钮
     back_button = tk.Button(root, text="Back to Menu",
                             command=lambda: show_operation_menu(root, student_service, subject_service))
     back_button.pack(pady=(5, 5))
 
 
 def show_change_password_menu(root, student_service, subject_service):
-    # 清空当前窗口的内容
     for widget in root.winfo_children():
         widget.destroy()
 
-    # 创建修改密码的界面
     tk.Label(root, text="Change Password").pack(pady=(10, 5))
 
     tk.Label(root, text="New Password:").pack(pady=(5, 0))
     new_password_entry = tk.Entry(root, show='*')
     new_password_entry.pack(pady=(0, 5))
-    new_password_entry.focus_set()  # 设置焦点到新密码输入框
+    new_password_entry.focus_set()
 
     tk.Label(root, text="Confirm Password:").pack(pady=(5, 0))
     confirm_password_entry = tk.Entry(root, show='*')
@@ -137,14 +124,13 @@ def show_change_password_menu(root, student_service, subject_service):
             new_password = new_password_entry.get()
             confirm_password = confirm_password_entry.get()
             if new_password == confirm_password:
-                # 在这里执行修改密码的逻辑
 
                 if not util.check_password_pattern(new_password):
                     messagebox.showerror("Error", "Incorrect password format.")
                 else:
                     student_service.change_password(new_password)
 
-                show_operation_menu(root, student_service, subject_service)  # 返回主菜单
+                show_operation_menu(root, student_service, subject_service)
             else:
                 messagebox.showerror("Error", "Password does not match - try again!")
         except Exception as e:
@@ -153,7 +139,6 @@ def show_change_password_menu(root, student_service, subject_service):
     confirm_button = tk.Button(root, text="Submit", command=confirm_change_password)
     confirm_button.pack(pady=(10, 5))
 
-    # 返回主菜单按钮
     back_button = tk.Button(root, text="Back to Menu",
                             command=lambda: show_operation_menu(root, student_service, subject_service))
     back_button.pack(pady=(5, 5))
